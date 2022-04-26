a46a9820ae16659425e0c0646e81c508ecb92223
damian <danslik@interactivebrokers.ie>
2022-04-26 16:32:02 +0100

    add media queries for different small and medium devices

---
+++ static/scripts/navbar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("navbar-drawer").style.width = "225px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("navbar-drawer").style.width = "0";
+  } 
