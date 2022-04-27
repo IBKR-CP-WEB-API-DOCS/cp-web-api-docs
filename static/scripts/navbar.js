2e2dd04e2493e35acb86de1775f55338cb88bd90
damian <danslik@interactivebrokers.ie>
2022-04-27 13:57:08 +0100

    dynamically set active class

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -1,9 +1,16 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("navbar-drawer").style.width = "225px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("navbar-drawer").style.width = "0";
-  } 
+  document.getElementById("navbar-drawer").style.width = "225px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("navbar-drawer").style.width = "0";
+}
+
+$(function () {
+  let currentPage = window.location.pathname;
+  $('#navbar-top').find('a').each(function () {
+    $(this).toggleClass('active', "/" + $(this).attr('href') == currentPage);
+  });
+});
