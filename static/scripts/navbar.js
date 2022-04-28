aca156d896f16ac4ea802dda02f852181ff1e1f7
damian <danslik@interactivebrokers.ie>
2022-04-28 09:53:35 +0100

    navbar and drawer navigation

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -1,19 +1,17 @@
-/* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("navbar-drawer").style.width = "225px";
+  document.getElementById("navbar-drawer").style.width = "200px";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("navbar-drawer").style.width = "0";
 }
 
+// This function adds the active class to the currently opened page - used as visual indicator for the user
 $(function () {
   let currentPage = window.location.pathname.split("/").slice(-1)[0];
   $('#navbar-top')
     .find('a')
     .each(function () {
-      console.log("/"+$(this).attr('href'))
       $(this).toggleClass('active', $(this).attr('href') == currentPage);
   });
 });
--- static/scripts/site.js
+++
@@ -1,5 +0,0 @@
-// Dynamically add the navbar
-$(function () {
-    $("#navbar")
-    .load('./components/navbar.html')
-});
